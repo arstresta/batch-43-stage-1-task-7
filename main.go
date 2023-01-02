7dd55eb93a784404aafdfb33d9975c8cfbc77847
arstresta <121067598+arstresta@users.noreply.github.com>
2023-01-02 22:06:58 +0700

    Routing dan Get Post

---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"text/template"
+
+	"github.com/gorilla/mux"
+)
+
+var Data = map[string]interface{}{
+	"Title":   "Personal Web",
+	"isLogin": false,
+}
+
+func main() {
+	route := mux.NewRouter()
+	route.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
+
+	route.HandleFunc("/", helloWorld).Methods("GET")
+	route.HandleFunc("/home", home).Methods("GET").Name("home")
+	route.HandleFunc("/project", project).Methods("GET")
+	route.HandleFunc("/project/{id}", projectDetail).Methods("GET")
+	route.HandleFunc("/project", addProject).Methods("POST")
+	route.HandleFunc("/contact", contact).Methods("GET")
+
+	fmt.Println("Server Is Running On Port 5000")
+	http.ListenAndServe("localhost:5000", route)
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello World"))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	var tmpl, err = template.ParseFiles("views/index.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Message: " + err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	tmpl.Execute(w, Data)
+}
+
+func project(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	var tmpl, err = template.ParseFiles("views/add-project.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Message" + err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	tmpl.Execute(w, Data)
+}
+
+func projectDetail(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	var tmpl, err = template.ParseFiles("views/detail-project.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Message" + err.Error()))
+		return
+	}
+
+	resp := map[string]interface{}{
+		"Data": Data,
+		"Id":   id,
+	}
+
+	w.WriteHeader(http.StatusOK)
+	tmpl.Execute(w, resp)
+}
+
+func addProject(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Title : " + r.PostForm.Get("name"))
+
+	http.Redirect(w, r, "/project", http.StatusMovedPermanently)
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	var tmpl, err = template.ParseFiles("views/contact-form.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Message" + err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	tmpl.Execute(w, Data)
+}
